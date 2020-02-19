a9565ca95757d1687b702a7f1c021977e0872cf2
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-02-19 12:35:58 -0500

    Update to use kubeconfig

--- consul/main.tf
+++ consul/main.tf
@@ -25,12 +25,6 @@ provider "local" {
 provider "helm" {
   version = "~> 1.0"
   kubernetes {
-    host     = data.terraform_remote_state.cluster.outputs.host
-    username = data.terraform_remote_state.cluster.outputs.username
-    password = data.terraform_remote_state.cluster.outputs.password
-
-    client_certificate     = data.terraform_remote_state.cluster.outputs.client_certificate
-    client_key             = data.terraform_remote_state.cluster.outputs.client_key
-    cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
+    config_path = "/home/terraform/.kube/config"
   }
 }
