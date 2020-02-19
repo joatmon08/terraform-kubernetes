55a48391ea71a22c83ffda53575750df1fb7aab2
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-02-19 10:04:37 -0500

    Update outputs with Kubernetes cluster

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -1,5 +1,20 @@
 output "cluster" {
-  value     = azurerm_kubernetes_cluster.engineering.name
+  value = azurerm_kubernetes_cluster.engineering.name
+}
+
+output "host" {
+  value     = azurerm_kubernetes_cluster.engineering.kube_config.0.host
+  sensitive = true
+}
+
+output "password" {
+  value     = azurerm_kubernetes_cluster.engineering.kube_config.0.password
+  sensitive = true
+}
+
+output "username" {
+  value     = azurerm_kubernetes_cluster.engineering.kube_config.0.username
+  sensitive = true
 }
 
 output "client_certificate" {
@@ -7,7 +22,12 @@ output "client_certificate" {
   sensitive = true
 }
 
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.engineering.kube_config_raw
+output "client_key" {
+  value     = azurerm_kubernetes_cluster.engineering.kube_config.0.client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.engineering.kube_config.0.cluster_ca_certificate
   sensitive = true
 }
