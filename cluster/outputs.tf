263ba53f0a9b8b544f5329101e41bbd0d592f124
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-02-19 10:47:27 -0500

    Update kubeconfig in outputs

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -31,3 +31,8 @@ output "cluster_ca_certificate" {
   value     = azurerm_kubernetes_cluster.engineering.kube_config.0.cluster_ca_certificate
   sensitive = true
 }
+
+output "kubeconfig" {
+  value     = azurerm_kubernetes_cluster.example.kube_config_raw
+  sensitive = true
+}
