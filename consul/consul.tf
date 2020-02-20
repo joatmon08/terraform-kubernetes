80cf3579046ee23797cf112347a68097e34122f4
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-02-20 13:00:01 -0500

    Update to use static credentials

--- consul/consul.tf
+++ consul/consul.tf
@@ -1,16 +1,7 @@
-resource "local_file" "kubeconfig" {
-  sensitive_content = data.terraform_remote_state.cluster.outputs.kubeconfig
-  filename          = var.config_path
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "helm_release" "consul" {
-  depends_on = [local_file.kubeconfig]
-  name       = var.environment
-  chart      = "${path.module}/consul-helm"
-  namespace  = var.namespace
+  name      = var.environment
+  chart     = "${path.module}/consul-helm"
+  namespace = var.namespace
 
   set {
     name  = "global.name"
--- consul/main.tf
+++ consul/main.tf
@@ -21,6 +21,12 @@ data "terraform_remote_state" "cluster" {
 provider "helm" {
   version = "~> 1.0"
   kubernetes {
-    config_path = var.config_path
+    host     = data.terraform_remote_state.cluster.outputs.host
+    username = data.terraform_remote_state.cluster.outputs.username
+    password = data.terraform_remote_state.cluster.outputs.password
+
+    client_certificate     = data.terraform_remote_state.cluster.outputs.client_certificate
+    client_key             = data.terraform_remote_state.cluster.outputs.client_key
+    cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
   }
 }
