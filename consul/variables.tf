a32ea38d9ab019b4f80aece4f925b971d17fdd01
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-02-28 11:56:12 -0500

    Update consul and vault variable descriptions

--- consul/variables.tf
+++ consul/variables.tf
@@ -1,7 +1,25 @@
-variable "environment" {}
-variable "namespace" {}
-variable "cluster_workspace" {}
-variable "organization" {}
+variable "environment" {
+  type        = string
+  description = "Environment to describe Consul Helm release"
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace to which to deploy the Consul Helm chart"
+}
+
+variable "cluster_workspace" {
+  type        = string
+  description = "Workspace that created the Kubernetes cluster"
+}
+
+variable "organization" {
+  type        = string
+  description = "Organization of workspace that created the Kubernetes cluster"
+}
+
 variable "replicas" {
-  default = 1
+  type        = number
+  default     = 1
+  description = "Number of consul replicas"
 }
--- vault/variables.tf
+++ vault/variables.tf
@@ -1,4 +1,19 @@
-variable "environment" {}
-variable "namespace" {}
-variable "cluster_workspace" {}
-variable "organization" {}
+variable "environment" {
+  type        = string
+  description = "Environment to describe Vault Helm release"
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace to which to deploy the Vault Helm chart"
+}
+
+variable "cluster_workspace" {
+  type        = string
+  description = "Workspace that created the Kubernetes cluster"
+}
+
+variable "organization" {
+  type        = string
+  description = "Organization of workspace that created the Kubernetes cluster"
+}
