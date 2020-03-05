76fa16f5800a54ecc16bc52f841bbf4072de29a5
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-03-05 18:46:35 -0500

    Don't use outputs

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -1,9 +1,9 @@
 output "cluster" {
-  value = var.cluster_provider == "gke" ? google_container_cluster.engineering.0.name : digitalocean_kubernetes_cluster.engineering.0.name
+  value = var.cluster_provider == "gke" ? google_container_cluster.engineering.name : digitalocean_kubernetes_cluster.engineering.0.name
 }
 
 output "host" {
-  value     = var.cluster_provider == "gke" ? google_container_cluster.engineering.0.endpoint : digitalocean_kubernetes_cluster.engineering.0.endpoint
+  value     = var.cluster_provider == "gke" ? google_container_cluster.engineering.endpoint : digitalocean_kubernetes_cluster.engineering.0.endpoint
   sensitive = true
 }
 
@@ -13,16 +13,16 @@ output "token" {
 }
 
 output "cluster_ca_certificate" {
-  value     = var.cluster_provider == "gke" ? base64decode(google_container_cluster.engineering.0.master_auth.0.cluster_ca_certificate) : base64decode(digitalocean_kubernetes_cluster.engineering.0.kube_config.0.cluster_ca_certificate)
+  value     = var.cluster_provider == "gke" ? base64decode(google_container_cluster.engineering.master_auth.0.cluster_ca_certificate) : base64decode(digitalocean_kubernetes_cluster.engineering.0.kube_config.0.cluster_ca_certificate)
   sensitive = true
 }
 
 output "username" {
-  value     = var.cluster_provider == "gke" ? google_container_cluster.engineering.0.master_auth.0.username : ""
+  value     = var.cluster_provider == "gke" ? google_container_cluster.engineering.master_auth.0.username : ""
   sensitive = true
 }
 
 output "password" {
-  value     = var.cluster_provider == "gke" ? google_container_cluster.engineering.0.master_auth.0.password : ""
+  value     = var.cluster_provider == "gke" ? google_container_cluster.engineering.master_auth.0.password : ""
   sensitive = true
 }
