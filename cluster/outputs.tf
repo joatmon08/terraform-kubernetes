9575a2d6bb867b4b6f933c212063928161eb2153
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-04-10 13:01:52 -0400

    Fix outputs to remove vault_consul

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -21,7 +21,3 @@ output "password" {
   value     = google_container_cluster.engineering.master_auth.0.password
   sensitive = true
 }
-
-output "enable_consul_and_vault" {
-  value = var.enable_consul_and_vault
-}
